d5ff1b2a215979eb966d0300ab95bee4acabaf59
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-08-24 01:31:12 -0500

    Added custom User & UserAdmin.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,54 @@
-from django.contrib import admin
+"""User admin classes."""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('pk', 'username', 'phone_number', 'website',
+                    'profile_picture')
+    list_display_links = ('pk', 'username')
+    list_editable = ('phone_number', 'website', 'profile_picture')
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password')
+        }),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'website', 'biography',
+                       'phone_number', 'profile_picture')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                       'user_permissions'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    add_fieldsets = ((None, {
+        'fields': ('username', 'email', 'password1', 'password2', 'website',
+                   'biography', 'phone_number', 'profile_picture')
+    }), )
+
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'phone_number',
+    )
+
+    list_filter = (
+        'is_active',
+        'is_staff',
+        'date_joined',
+        'last_modified',
+    )
+
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+"""Users models."""
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """User model."""
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    profile_picture = models.ImageField(upload_to='users/pictures',
+                                        blank=True,
+                                        null=True)
+
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Returns the username."""
+        return self.username
