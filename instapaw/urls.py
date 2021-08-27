c8cc42b5afa7d0848289458ad912169f618ed919
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-08-27 15:35:42 -0500

    Refactored users views.

--- instapaw/urls.py
+++ instapaw/urls.py
@@ -3,19 +3,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from posts import views as posts_views
-from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # posts views
     path('', posts_views.list_posts, name='feed'),
 
-    path('accounts/login/', users_views.login_view, name='login'),
-    path('accounts/logout/', users_views.logout_view, name='logout'),
-    path('accounts/signup/', users_views.signup, name='signup'),
-    path('accounts/edit/', users_views.edit_profile, name='edit_profile')
+    # users views
+    path('accounts/', include('users.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+"""Users URLs."""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('edit/', views.edit_profile, name='edit_profile'),
+]
